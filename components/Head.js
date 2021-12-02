ac9fe638b6a6dd2719cba1a8f347f256fe2b2008
Jehadabuawwad <jehadabuawwad@outlook.com>
2021-12-02 22:59:52 +0200

    fix : updating the Head component for secure issue

--- components/Head.js
+++ components/Head.js
@@ -5,8 +5,14 @@ export default function PageHead(){
     return(
 
         <Head>
-        <title>Create Next App</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
+        <title>Cookie Stand Application</title>
         <link rel="icon" href="/favicon.ico" />
         </Head>
     )
 }
+
+!
